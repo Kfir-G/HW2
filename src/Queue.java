39150795256e8c38bb702eaf3b2be1397a1920b9
Kfir-G <kfirg15@gmail.com>
2020-11-27 14:21:07 +0200

    add methods in GH

--- src/Queue.java
+++ src/Queue.java
@@ -1,13 +1,12 @@
 /**
  * This Class implements the state and behaviour of Queue infrastructure.
  * It inherits from DDLinkedList class.
- * @author Dr. Moshe Deutsch
  */
 public class Queue <T> extends DDLinkedList <T>
 {
 	/**
      * enqueue an element to the Back of the Queue.
-     * @param val the integer value to be added to the Back of the Queue.
+     * @param val the T value to be added to the Back of the Queue.
      */
 	public void enqueue(T val)
 	{
@@ -17,7 +16,7 @@ public class Queue <T> extends DDLinkedList <T>
 	/**
      * dequeue an element from the Front of the Queue.
      * @return the value of the element dequeued from the Front of the Queue. 
-     * If the Queue is empty, returns -1.
+     * If the Queue is empty, returns null.
      */
 	public T dequeue()
 	{
--- src/Stack.java
+++ src/Stack.java
@@ -1,13 +1,12 @@
 /**
  * This Class implements the state and behaviour of Stack infrastructure.
  * It inherits from DDLinkedList class.
- * @author Dr. Moshe Deutsch
  */
 public class Stack<T> extends DDLinkedList <T>
 {
 	/**
      * push an element to the top of the Stack.
-     * @param val the integer value to be added to the top of the Stack.
+     * @param val the T value to be added to the top of the Stack.
      */
 	public void push(T val)
 	{
@@ -17,7 +16,7 @@ public class Stack<T> extends DDLinkedList <T>
 	/**
      * pop an element from the top of the Stack.
      * @return the value of the element popped from the top of the Stack. If the
-     * Stack is empty, returns -1.
+     * Stack is empty, returns null.
      */
 	public T pop()
 	{
