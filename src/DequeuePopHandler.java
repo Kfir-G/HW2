e67b6ad5d098f0ea1a58a626a4a13b2070a3e23f
Kfir-G <kfirg15@gmail.com>
2020-11-29 17:03:17 +0200

    add method processRequest to DPH

--- src/DequeuePopHandler.java
+++ src/DequeuePopHandler.java
@@ -21,10 +21,29 @@ public class DequeuePopHandler extends GeneralHandler
         super(intSk);
     }
 
-    public void processRequest()
+    /**
+     * This method implements the abstract method "processRequest", inherited from GeneralHandler class. This method
+     * Dequeue/Pop an item from the Queue/Stack respectively, and presents it on via showMessageDialog method of
+     * JOptionPane. If the examined Queue/Stack is empty, issues an appropriate message to the user using
+     * showInputDialog method of JOptionPane Java swing class (input dialog GUI). Likewise, ALL user messages are
+     * performed via showMessageDialog method of JOptionPane Java swing class (message dialog GUI).
+     */
+    public void processRequest() //how to do it elegant? @omer?
     {
-        if(intQ != null && intQ.isEmpty())
-
-
+       if(intQ != null) //for Queue
+       {
+           if(intQ.isEmpty()) {
+               JOptionPane.showMessageDialog(null, "Queue is Empty!!");
+               return;  //EXIT
+           }
+           JOptionPane.showMessageDialog(null,"Value Dequeue from Queue is:"+intQ.dequeue());
+       }
+       else{    //for Stack
+           if(intSt.isEmpty()) {
+               JOptionPane.showMessageDialog(null, "Stack is Empty!!");
+               return;  //EXIT
+           }
+           JOptionPane.showMessageDialog(null,"Value Pop from Stack is:"+intSt.pop());
+       }
     }
 }
